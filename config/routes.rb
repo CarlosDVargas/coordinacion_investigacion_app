8ac1faa7ecb7abfc8f05b4c34734101a05139abd
Carlos Daniel Vargas Varela <carlos.vargas@ucrso.info>
2022-04-03 15:19:56 -0600

    Se agrega los controladores y vistas de la entidad User

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+                     sessions: "users/sessions",
+                     registrations: "users/registrations",
+                   }
   resources :projects
   resources :investigators
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
